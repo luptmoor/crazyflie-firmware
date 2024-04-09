31fc33cd7be521ded36424b2e0c42c64d2cd9124
luptmoor <lukas.uptmoor@web.de>
2024-04-09 16:07:57 +0200

    Removed take-off procedure and command sharing etc

--- src/modules/src/relative_control.c
+++ src/modules/src/relative_control.c
@@ -16,17 +16,11 @@
 #define USE_MONOCAM 0
 
 static bool isInit;
-static bool onGround = true;
-static bool keepFlying = false;
+static bool keepFlying = true;
 static setpoint_t setpoint;
 static float relaVarInCtrl[NumUWB][STATE_DIM_rl];
 static float inputVarInCtrl[NumUWB][STATE_DIM_rl];
 static uint8_t selfID;
-static float height;
-static float initial_hover_height;
-
-static float kf_init_layer_distance = 0.30f; // [m]
-static uint8_t layer_number;
 
 static uint8_t in_sight = true;
 static float datum_x = 0.0f;
@@ -34,12 +28,9 @@ static float datum_y = 0.0f;
 static float datum_z = 0.0f;
 static uint8_t datum_id = 0;
 
-
 static float relaCtrl_p = 2.0f;
 static float relaCtrl_i = 0.0001f;
 static float relaCtrl_d = 0.01f;
-// static float NDI_k = 2.0f;
-static char c = 0; // monoCam
 
 
 static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, float yawrate) {
@@ -55,8 +46,7 @@ static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, 
   commanderSetSetpoint(setpoint, 3);
 }
 
-static void set3DVel(setpoint_t *setpoint, float vx, float vy, float vz, float yawrate)
-{
+static void set3DVel(setpoint_t *setpoint, float vx, float vy, float vz, float yawrate) {
   setpoint->mode.z = modeVelocity;
   setpoint->mode.yaw = modeVelocity;
   setpoint->attitudeRate.yaw = yawrate;
@@ -118,69 +108,23 @@ static void formation0asCenter(float tarX, float tarY, float tarZ) {
   pid_vy = constrain(pid_vy, -1.5f, 1.5f);
   pid_vz = constrain(pid_vz, -0.3f, 0.3f);
 
-  // float rep_x = 0.0f;
-  // float rep_y = 0.0f;
-  // for(uint8_t i=0; i<NumUWB; i++){
-  //   if(i!=selfID){
-  //     float dist = relaVarInCtrl[i][STATE_rlX]*relaVarInCtrl[i][STATE_rlX] + relaVarInCtrl[i][STATE_rlY]*relaVarInCtrl[i][STATE_rlY];
-  //     dist = sqrtf(dist);
-  //     rep_x += -0.5f * (SIGN(0.5f - dist) + 1) / (abs(relaVarInCtrl[i][STATE_rlX]) + 0.001f) * SIGN(relaVarInCtrl[i][STATE_rlX]);
-  //     rep_y += -0.5f * (SIGN(0.5f - dist) + 1) / (abs(relaVarInCtrl[i][STATE_rlY]) + 0.001f) * SIGN(relaVarInCtrl[i][STATE_rlY]);
-  //   }
-  // }
-  // rep_x = constrain(rep_x, -1.5f, 1.5f);
-  // rep_y = constrain(rep_y, -1.5f, 1.5f);
-
-  // pid_vx = constrain(pid_vx + rep_x, -1.5f, 1.5f);
-  // pid_vy = constrain(pid_vy + rep_y, -1.5f, 1.5f);
-
   set3DVel(&setpoint, pid_vx, pid_vy, pid_vz, 0);
 }
 
 
 void relativeControlTask(void* arg) {
-  static uint32_t takeoff_tick;
   systemWaitStart();
-  static logVarId_t logIdStateIsFlying;
-  logIdStateIsFlying = logGetVarId("kalman", "inFlight");
-
 
   // Main Loop
   while(1) {
-
-    //Check if we should fly
     vTaskDelay(10);
-    if(selfID==0){
-      keepFlying = logGetUint(logIdStateIsFlying);
-      keepFlying = command_share(selfID, keepFlying);
-      continue; // Do not send commands to leader drone, as it is manually controlled
+
+    if(in_sight){
+      continue; // Do not do anything if in sight of AR headset
     }
-
-    keepFlying = command_share(selfID, keepFlying);
-
 
     // If we should fly and information can be read from the localisation
     if(relativeInfoRead((float *)relaVarInCtrl, (float *)inputVarInCtrl) && keepFlying) {
-
-
-      // TAKE OFF if on ground
-      if(onGround) {
-        estimatorKalmanInit(); // reseting kalman filter
-        vTaskDelay(M2T(2000));
-        for (int i=0; i<50; i++) {
-          setHoverSetpoint(&setpoint, 0, 0, initial_hover_height, 0);
-          vTaskDelay(M2T(100));
-        }
-        onGround = false;
-
-        // Define time datum
-        takeoff_tick = xTaskGetTickCount();
-        // Initialise RNG differently for each drone
-        srand(selfID * takeoff_tick);
-        layer_number = selfID;
-        height = initial_hover_height + layer_number * kf_init_layer_distance;
-      }
-
 
       // Hold relative Position to leader drone. CONVERGENCE ASSUMED
       targetX = relaVarInCtrl[0][STATE_rlX];
@@ -194,15 +138,12 @@ void relativeControlTask(void* arg) {
 
     // If we should not fly: LANDING procedure
     } else {
-      if(!onGround){
-        for (int i=1; i<5; i++) {
-          if(selfID!=0){
-          setHoverSetpoint(&setpoint, 0, 0, 0.3f-(float)i*0.05f, 0);
-          vTaskDelay(M2T(10));
-          }
+      for (int i=1; i<5; i++) {
+        if(selfID!=0){
+        setHoverSetpoint(&setpoint, 0, 0, 0.3f-(float)i*0.05f, 0);
+        vTaskDelay(M2T(10));
         }
-        onGround = true;
-      } 
+      }
     }
 
   }
@@ -216,12 +157,8 @@ void relativeControlInit(void) {
   selfID = (uint8_t)(((configblockGetRadioAddress()) & 0x000000000f) - 5);
 
   xTaskCreate(relativeControlTask,"relative_Control",configMINIMAL_STACK_SIZE, NULL, 3, NULL);
-  height = 1.0f;
-  initial_hover_height = 0.8f;
-
   isInit = true;
 }
-
 
 
 
@@ -240,9 +177,5 @@ PARAM_ADD(PARAM_FLOAT, datum_y, &datum_y)
 PARAM_ADD(PARAM_FLOAT, datum_z, &datum_z)
 PARAM_ADD(PARAM_UINT8, datum_id, &datum_id)
 
-
 PARAM_GROUP_STOP(arswarm)
 
-LOG_GROUP_START(mono_cam)
-LOG_ADD(LOG_UINT8, charCam, &c)
-LOG_GROUP_STOP(mono_cam)
